3a4515e61759c3e77d2cd900a2087532311793a1
ANONYMOUSx46 <liamjudedewet85@gmail.com>
2025-02-15 23:35:35 +0200

    Add files via upload

---
+++ bruteforce2.py
@@ -0,0 +1,29 @@
+'''
+Forage AIG Cybersecurity Program 
+Bruteforce template
+'''
+
+from zipfile import ZipFile
+
+# Attempt to extract the zip file with the given password
+def attempt_extract(zf_handle, password):
+    try:
+        zf_handle.extractall(pwd=password.encode())
+        print(f"[+] Password found: {password}")
+        return True
+    except:
+        return False
+
+def main():
+    print("[+] Beginning bruteforce")
+    with ZipFile('enc.zip') as zf:
+        with open('rockyou.txt', 'rb') as f:
+            for line in f:
+                password = line.strip().decode()
+                if attempt_extract(zf, password):
+                    print("[+] Password found!")
+                    return
+            print("[+] Password not found in list")
+
+if __name__ == "__main__":
+    main()
